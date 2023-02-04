18c93de75c8b10ae294e40fa75eda8cb2066d6a9
rithviksonwalkar <rsonwalk@gmail.com>
2023-02-03 19:25:03 -0600

    Changed P0 NoRisk, LowRiskStation, and LowRiskPlace files to work with P1

---
+++ src/main/java/frc/robot/commands/P1/LowRiskPlace.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.P1;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.commands.common.AprilTagAlign;
+import frc.robot.commands.common.PathWeave;
+import frc.robot.commands.common.SequentialCommand;
+import frc.robot.subsystems.PhotonVision;
+import frc.robot.subsystems.Swerve;
+
+public class LowRiskPlace extends SequentialCommand {
+
+    // Routine that scores preloaded game piece, then exits the community for mobility points
+    public LowRiskPlace(Swerve swerve, PhotonVision vision, boolean isBlue) {
+        super(isBlue);
+        addCommands(new AprilTagAlign(swerve, vision, isBlue ? 8 : 1,
+                isBlue ? AprilTagAlign.TagAlignment.LEFT : AprilTagAlign.TagAlignment.RIGHT));
+        // addCommands(placement command);
+        addCommands(PathWeave.fromFieldCoordinates(swerve,
+                new Pose2d(new Translation2d(5.6896, 0.849 * yMult), new Rotation2d()), new Translation2d(5.6896, 0.849 * yMult)));
+    }
+
+}
---
+++ src/main/java/frc/robot/commands/P1/LowRiskStation.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.P1;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.commands.common.ChargeStationAlign;
+import frc.robot.commands.common.PathWeave;
+import frc.robot.commands.common.SequentialCommand;
+import frc.robot.subsystems.Swerve;
+
+import java.util.function.DoubleSupplier;
+
+public class LowRiskStation extends SequentialCommand {
+
+    public LowRiskStation(Swerve swerve, DoubleSupplier roll, boolean isBlue) {
+        super(isBlue);
+        addCommands(
+                PathWeave.fromFieldCoordinates(swerve, new Pose2d(new Translation2d(4.489, 2.079 * yMult),
+                        Rotation2d.fromDegrees(180)), new Translation2d(4.489, 1.241 * yMult)),
+                new ChargeStationAlign(swerve, roll)
+        );
+    }
+}
