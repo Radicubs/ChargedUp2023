fae840454bd1881630819b464d10e9056ce3eb63
rithviksonwalkar <rsonwalk@gmail.com>
2023-02-03 20:32:29 -0600

    Completed LowRiskPlace and LowRiskStation for P2

---
+++ src/main/java/frc/robot/commands/P2/LowRiskStation.java
@@ -0,0 +1,23 @@
+package frc.robot.commands.P2;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.commands.common.ChargeStationAlign;
+import frc.robot.commands.common.PathWeave;
+import frc.robot.commands.common.SequentialCommand;
+import frc.robot.subsystems.Swerve;
+
+import java.util.function.DoubleSupplier;
+
+public class LowRiskStation extends SequentialCommand {
+
+    public LowRiskStation(Swerve swerve, DoubleSupplier roll, boolean isBlue) {
+        super(isBlue);
+        addCommands(
+                PathWeave.fromRelativeCoordinates(swerve, new Pose2d(new Translation2d(0, 0 * yMult),
+                        Rotation2d.fromDegrees(180))),
+                new ChargeStationAlign(swerve, roll)
+        );
+    }
+}
